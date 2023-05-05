2423cddec65d2736b057c25a369e39ea8f65b23e
tavolop3 <tavo.lop33@gmail.com>
2023-05-05 16:21:10 -0300

    Primer commit

---
+++ src/test/java/com/ohmydog/veterinaria/ProyectoVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohmydog.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
